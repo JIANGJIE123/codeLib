e40056ee49c2c56c4d6049ba372886f6e5aedc92
JINAGJIE123 <1429865799@qq.com>
2018-10-23 00:59:51 +0800

    guofubaoli4

--- guofubaoli/js/index.js
+++ guofubaoli/js/index.js
@@ -5,6 +5,11 @@ var _scroll =function () {
     } else {
         $(".top_box").removeClass("topFixed");
     };
+    $(".set_top").click(function () {
+        console.log("jiangjie")
+        $('html,body').stop().animate({"scrollTop":"0px"},1000);
+        return false;
+    })
 };
  $(".subtitle").animate({"margin-top":"80px"},"slow")
 $(document).ready($(window).scroll(_scroll))

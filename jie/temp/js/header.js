0084012d968b26eaf6629aee447a9ae403cd7c1c
Jessica <1429865799@qq.com>
2019-02-16 17:41:56 +0800

    每日一文首页声音内容

---
+++ jie/temp/js/header.js
@@ -0,0 +1,20 @@
+var body=document.body;
+var header=`
+    <header class="clearFloat">
+    <div class="hd_l clearFloat">
+        <div class="logo"><a href="index.html">每日一文</a></div>
+        <ul class="nv clearFloat">
+            <li><a href="index.html" class="activenv">文章</a></li>
+            <li><a href="voice.html">声音</a></li>
+            <li><a href="#">书架</a></li>
+        </ul>
+    </div>
+    <div class="hd_r">
+        <a href="" class="hd_r_link">
+            <em class="hd_r_icon"></em>
+            <span class="hr_r_text">加关注</span>
+        </a>
+    </div>
+</header>
+`;
+body.innerHTML=header;
